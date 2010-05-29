1c63c9550c535b4db50024562c4a94fb37c33e80
Chase Southard <chase.southard@gmail.com>
2010-05-29 10:11:58 -0400

    setting up paperclip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   attr_accessible :title
+  
+  has_attached_file :picture, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :path => ":attachment/:id/:style.:extension", :url => ":attachment/:id/:style.:extension"
 end
