0941ca8423ebea1d95c70799c717c9e333a8eab5
Chase Southard <chase.southard@gmail.com>
2010-05-31 23:21:01 -0400

    created listener

---
+++ app/controllers/listener_controller.rb
@@ -0,0 +1,29 @@
+class ListenerController < ApplicationController
+  
+  skip_before_filter :verify_authenticity_token
+  
+  def receive_email
+    @params = params
+    
+    @inbound_email = Post.new(:title => clean_field(params["subject"]),
+                              :picture => params["attachment1"]
+                              )
+                          
+    respond_to do |format|
+      if @inbound_email.save && request.post?
+        flash[:notice] = 'Sucessful Post.'
+        format.xml { render => @inbound_email.xml, :status => :created  }
+      else
+        flash[:error] = 'There was an error with saving the post'
+        format.xml { render => @inbound_email.xml.errors, :status => :unprocessbile_entry }
+      end
+    end
+  
+  end
+  
+  private
+  def clean_field(input_string)
+    input_string.gsub(/\n/, '') if input_string
+  end
+  
+end
